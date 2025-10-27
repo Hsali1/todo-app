3efe7407667f6bf7184d16e33b2f4644d11bbb35
Hassan Ali <Hassanalisid1996@gmail.com>
2025-10-27 15:00:31 -0700

    update readme

--- functions.py
+++ functions.py
@@ -1,11 +1,14 @@
-def read_file(filepath = "todos.txt"):
+FILEPATH = "todos.txt"
+
+def read_file(filepath=FILEPATH):
     """ Reads a text file and return list of to-do items. """
     with open(filepath, 'r') as file:
         todos = file.readlines()
     return todos
 
 
-def write_file(todo_list):
+def write_file(todo_list, filepath=FILEPATH):
     """ Write the to-do items list in the text file """
-    with open("todos.txt", 'w') as file:
+    with open(filepath, 'w') as file:
         file.writelines(todo_list)
+# some random stuff
