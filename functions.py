983252768642fe2395c528f042e9e38ceaec6239
Hassan Ali <Hassanalisid1996@gmail.com>
2025-11-02 20:05:41 -0800

    implement the edit button

--- functions.py
+++ functions.py
@@ -1,14 +1,17 @@
 FILEPATH = "todos.txt"
 
 def read_file(filepath=FILEPATH):
-    """ Reads a text file and return list of to-do items. """
-    with open(filepath, 'r') as file:
-        todos = file.readlines()
-    return todos
-
+    try:
+        with open(filepath, 'r') as file:
+            # remove the trailing newline for clean display
+            return [line.strip('\n') for line in file.readlines()]
+    except FileNotFoundError:
+        return []
 
 def write_file(todo_list, filepath=FILEPATH):
     """ Write the to-do items list in the text file """
     with open(filepath, 'w') as file:
-        file.writelines(todo_list)
-# some random stuff
+        # re-add newline for each todo before writing
+        file.writelines(f"{todo}\n" for todo in todo_list)
+
+        
--- gui.py
+++ gui.py
@@ -2,25 +2,47 @@ import functions
 import FreeSimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo", key="todo")
+input_box = sg.InputText(tooltip="Enter todo", key="new_todo")
 add_button = sg.Button("Add")
 
+list_box = sg.Listbox(values=functions.read_file(), 
+                      key="todo_list", 
+                      enable_events=True, 
+                      size=[45, 10])
+edit_button = sg.Button("Edit")
+
 window = sg.Window('My To-Do App', 
-                   layout=[[label], [input_box, add_button]], 
+                   layout=[[label], 
+                           [input_box, add_button], 
+                           [list_box, edit_button]], 
                    font=("Helvetica", 20))
 
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
+    print("event, values = window.read()")
+    print(f"event = {event}")
+    print(f"values = {values}")
+    print(f"values[todo_list] = {values['todo_list']}")
     match event:
         case "Add":
             todos = functions.read_file()
-            new_todo = values["todo"] + "\n"
+            new_todo = values["new_todo"]
             todos.append(new_todo)
+            functions.write_file(todos)
+            window["todo_list"].update(values=todos)
+        case "Edit":
+            todo_to_edit = values["todo_list"][0]
+            new_todo = values["new_todo"]
+
+            todos = functions.read_file()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
 
             functions.write_file(todos)
+            window["todo_list"].update(values=todos)
+        case "todo_list":
+            window["new_todo"].update(value=values["todo_list"][0])
         case sg.WIN_CLOSED:
             break
-        
+
 window.close()
